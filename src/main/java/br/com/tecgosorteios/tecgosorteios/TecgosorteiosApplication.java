4de13e80f2c79d03790abb02892f882752c2dcec
Rúben Filipe Andrade de Oliveira <rubenfilipeao@gmail.com>
2022-01-11 00:28:48 -0300

    Commit inicial

---
+++ src/main/java/br/com/tecgosorteios/tecgosorteios/TecgosorteiosApplication.java
@@ -0,0 +1,13 @@
+package br.com.tecgosorteios.tecgosorteios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TecgosorteiosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TecgosorteiosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/tecgosorteios/tecgosorteios/TecgosorteiosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.tecgosorteios.tecgosorteios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TecgosorteiosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
